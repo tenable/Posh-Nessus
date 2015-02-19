d09e85f33d5688e32859281f09667e75a5b07052
Carlos Perez <carlos_perez@darkoperator.com>
2015-02-19 09:50:27 -0400

    Scan host details function with proper format file for object data.

--- Posh-Nessus.psm1
+++ Posh-Nessus.psm1
@@ -1289,6 +1289,92 @@ function Show-NessusScanDetail
     End{}
 }
 
+
+<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+function Show-NessusScanHostDetail
+{
+    [CmdletBinding()]
+    Param
+    (
+        # Nessus session Id
+        [Parameter(Mandatory=$true,
+                   Position=0,
+                   ValueFromPipelineByPropertyName=$true)]
+        [Alias('Index')]
+        [int32[]]
+        $SessionId = @(),
+
+        [Parameter(Mandatory=$true,
+                   Position=1,
+                   ValueFromPipelineByPropertyName=$true)]
+        [int32]
+        $ScanId,
+
+        [Parameter(Mandatory=$true,
+                   Position=2,
+                   ValueFromPipelineByPropertyName=$true)]
+        [int32]
+        $HostId,
+
+        [Parameter(Mandatory=$false,
+                   Position=3,
+                   ValueFromPipelineByPropertyName=$true)]
+        [Int32]
+        $HistoryId 
+    )
+
+    Begin{}
+    Process
+    {
+        $ToProcess = @()
+
+        foreach($i in $SessionId)
+        {
+            $Connections = $Global:NessusConn
+            
+            foreach($Connection in $Connections)
+            {
+                if ($Connection.SessionId -eq $i)
+                {
+                    $ToProcess += $Connection
+                }
+            }
+        }
+        $Params = @{}
+
+        if($HistoryId)
+        {
+            $Params.Add('history_id', $HistoryId)
+        }
+
+        foreach($Connection in $ToProcess)
+        {
+            $ScanDetails =  InvokeNessusRestRequest -SessionObject $Connection -Path "/scans/$($ScanId)/hosts/$($HostId)" -Method 'Get' -Parameter $Params
+
+            if ($ScanDetails)
+            {                
+                $HostProps = [ordered]@{}
+                $HostProps.Add('Info', $ScanDetails.info)
+                $HostProps.Add('Vulnerabilities', $ScanDetails.vulnerabilities)
+                $HostProps.Add('Compliance', $ScanDetails.compliance)
+                $HostObj = New-Object -TypeName psobject -Property $HostProps
+                $HostObj.pstypenames[0] = 'Nessus.Scan.HostDetails'
+                $HostObj             
+            }
+        }
+    }
+    End{}
+}
+
 <#
 .Synopsis
    Short description
